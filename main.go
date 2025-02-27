b18f5ce21db87ecacce2f1410e93b43dd77c224a
rifky.rachman <rifky.rachman@gmail.com>
2025-02-27 21:59:08 +0700

    test v1

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
 
 func main() {
 	http.HandleFunc("/home", homePage)
-
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal(err)
